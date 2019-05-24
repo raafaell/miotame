0e565c87d849006393c7e5748380030f12001001
Thoralf-M <thoralf.mue@gmail.com>
2019-05-24 23:24:15 +0200

    refactoring

--- js/app.js
+++ js/app.js
@@ -2,107 +2,83 @@ var iota = core.composeAPI({
   provider: 'https://nodes.thetangle.org:443'
 });
 
-// let urltag = window.location.pathname.slice(1, 10)
-// if (urltag.length != 9) {
-//   console.log("No shorturl found, proceed...")
-// } else {
-//   getAddressWithTag(urltag)
-// }
+let urltag = window.location.pathname.slice(1, 10)
+if (urltag.length != 9) {
+  console.log('No shorturl found, proceed...')
+} else {
+  getAddressWithChecksum(urltag)
+}
 
-async function sendTag(address) {
+async function sendTransaction() {
   try {
-    var address = address
+    let address = document.getElementById("UserAddress").value;
+    //remove spaces
+    address = address.replace(/\s/g, '')
     if (address.length != 90) {
-      console.log("Invalid address length")
-      document.getElementById("error").innerHTML = "Your address is not invalid";
-      return
+      return error('Invalid address length')
     }
-    var tag = address.slice(81, 90)
-    console.log("Tag: " + tag);
-    if (address.slice(81, 90) != checksum.addChecksum(address.slice(0, 81)).slice(81, 90)) {
-      console.log("Invalid checksum");
-      document.getElementById("error").innerHTML = "The checksum does not match";
-      return
+    let addressChecksum = checksum.addChecksum(address.slice(0, 81)).slice(81, 90)
+    if (address.slice(81, 90) != addressChecksum) {
+      return error('Invalid checksum')
     }
-    var transfers = [{
+    //send transaction with checksum as tag
+    let transfers = [{
       address: address,
       value: 0,
-      tag: tag,
-      message: 'WCHDHDDDGDDBTATAADXCCDHDPCSAADTCTA'
+      tag: addressChecksum,
+      message: converter.asciiToTrytes('https://miota.me/')
     }]
-
     let trytes = await iota.prepareTransfers((seed = '9'.repeat(81)), transfers)
     let bundle = await iota.sendTrytes(trytes, (depth = 3), (minWeightMagnitude = 14))
-    console.log(`Transaction sent: https://thetangle.org/transaction/${bundle[0].hash}`)
+    console.log(`Transaction with sent: https://thetangle.org/transaction/${bundle[0].hash}`)
+
+    //update website elements
+    let element = document.getElementById("AddressInput");
+    element.classList.add("hide");
+    let link = document.createElement('a');
+    link.textContent = 'https://miota.me/' + address.slice(81, 90);
+    link.href = 'https://miota.me/' + address.slice(81, 90);
+    document.getElementById('urldata').appendChild(link);
   }
   catch (err) {
     console.log(err)
+    error(err)
   }
 }
 
-async function getAddressWithTag(tag) {
+async function getAddressWithChecksum(tag) {
   try {
+    await new Promise(resolve => setTimeout(resolve, 1))
     if (!tag.match(/^[A-Z9]{9}$/)) {
-      console.error("Invalid tag")
-      document.getElementById("error").innerHTML = "This shorturl is not valid";
-      return
+      return error('Invalid tag')
     }
     let hashesWithTag = await iota.findTransactions({ tags: [tag] })
     let txObjects = await iota.getTransactionObjects(hashesWithTag)
-    let results = []
+    let matchingAdresses = []
+    //check for correct address
     txObjects.forEach(tx => {
       let addressWithChecksum = checksum.addChecksum(tx.address)
       if (addressWithChecksum.slice(81, 90) == tag) {
-        results.push(addressWithChecksum)
+        matchingAdresses.push(addressWithChecksum)
       }
     })
+    let results = [... new Set(matchingAdresses)]
     if (results.length == 0) {
-      console.log("No matching tx found with tag: " + tag)
-      Swal.fire(
-        `No matching tx found with tag:`,
-        tag,
-        'error'
-      )
-      return
-    }
-    let equal = results.every((val, i, arr) => val === arr[0])
-    if (equal == true) {
-      console.log("Address found: " + results[0]);
-      document.getElementById("urldata").innerHTML = tags[0].address;
+      return error(`No matching transaction found with tag: ${tag}`)
+    } else if (results.length > 1) {
+      console.error('Different addresses found: ' + array)
+      return error('Different addresses found, ask for a new one!')
+    } else if (results.length == 1) {
+      console.log('Address found: ' + results[0]);
       drawQR(results[0])
       let deeplink = document.getElementById("deeplink")
       deeplink.href = "iota://" + results[0];
       deeplink.style.display = "block";
-    } else {
-      console.error("Different addresses found: " + array)
-      document.getElementById("error").innerHTML = "Different addresses found: " + array;
     }
   }
   catch (err) {
-    console.error(err)
-  }
-}
-
-
-async function runApp() {
-  try {
-    let address = document.getElementById("UserAddress").value;
-    if (address.slice(81, 90) != checksum.addChecksum(address.slice(0, 81)).slice(81, 90)) {
-      document.getElementById("error").innerHTML = "Your address is invalid";
-      return
-    } else
-      var element = document.getElementById("AddressInput");
-    element.classList.add("hide");
-    var link = document.createElement('a');
-    link.textContent = 'https://miota.me/' + address.slice(81, 90);
-    link.href = address.slice(81, 90);
-    document.getElementById('urldata').appendChild(link);
-    await sendTag(address)
-    await new Promise(resolve => setTimeout(resolve, 1000))
-    await getAddressWithTag(address.slice(81, 90))
-  }
-  catch (e) {
-    console.log(e)
+    console.log(err)
+    error(err)
   }
 }
 
@@ -120,4 +96,12 @@ function drawQR(address) {
   var border = 1;
   var scale = 4
   qr.drawCanvas(scale, border, canvas);
+}
+
+function error(errorMessage) {
+  Swal.fire(
+    errorMessage,
+    '',
+    'error'
+  )
 }
